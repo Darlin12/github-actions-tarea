d0be8f37915d16e28d74814d9f09533cd1a1e4fd
darli <darlinmanuelc05@gmail.com>
2023-08-14 15:21:14 -0400

    ejecucion de pruebas añadido

--- test/hello.spec.js
+++ test/hello.spec.js
@@ -1,6 +1,6 @@
 describe('Hello Node.js App Test', () => {
     it('should display the correct message', () => {
-        browser.get('/');
+        browser.get('https://practixsao21hds.surge.sh/');
         const message = element(by.tagName('h1')).getText();
         expect(message).toContain('Hello, Node.js!');
     });
